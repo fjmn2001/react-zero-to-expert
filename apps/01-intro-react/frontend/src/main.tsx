9ef75b91573e679b2dd4809e15eaf3863ad475cf
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-02-28 15:23:31 -0400

    feat: adding eslint + prettier v1. Ref sn

--- apps/01-intro-react/frontend/src/main.tsx
+++ apps/01-intro-react/frontend/src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom"
 
-const name = `Francisco`
-const h1Tag = <h1>Hi there! { name }</h1>
+const name = "Francisco"
+const h1Tag = <h1>Hi there! {name}</h1>
 
-ReactDOM.render(
-    h1Tag,
-    document.getElementById('root')
-)
+ReactDOM.render(h1Tag, document.getElementById("root"))
