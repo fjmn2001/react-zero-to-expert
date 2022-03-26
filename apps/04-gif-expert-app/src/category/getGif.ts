e5c53efb7d0b0b5cf549a1a1e4add3468050b9f3
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-03-26 17:13:27 -0400

    feat: adding getGif test. Ref sn

--- apps/04-gif-expert-app/src/category/getGif.ts
+++ apps/04-gif-expert-app/src/category/getGif.ts
@@ -1,11 +1,13 @@
+import axios from "axios"
+
 const apiKey = "g13oXIzvbtzxNnCw6hjWVR31UjL0yGVw"
 
 export const getGifs = async (category: string) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${encodeURI(
     category
   )}&limit=10`
-  const response = await fetch(url)
-  const { data } = await response.json()
+  const response = await axios(url)
+  const { data } = response.data
 
   return data.map(
     ({
