c7fec9154db650a5d2a582da4961553f753e1136
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-03-30 21:19:46 -0400

    feat: adding GifGrid test v1. Ref sn

---
+++ apps/04-gif-expert-app/src/tests/category/GifGrid.test.tsx
@@ -0,0 +1,40 @@
+import { beforeEach, describe, expect, MockedFunction, test, vi } from "vitest"
+import { shallow } from "enzyme"
+import GifGrid from "../../category/GifGrid"
+import { useFetchGif } from "../../category/useFetchGif"
+import { Image } from "../../category/types"
+
+vi.mock("../../category/useFetchGif")
+
+describe("Testing <GifGrid/>", () => {
+  const category = "One Punch"
+
+  beforeEach(() => {
+    vi.resetAllMocks()
+  })
+
+  test("it should show correctly", () => {
+    ;(useFetchGif as MockedFunction<any>).mockReturnValue({
+      data: [],
+      loading: true,
+    })
+    const wrapper = shallow(<GifGrid category={category} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test("it should show items when images have been loaded", () => {
+    const gifs: Image[] = [
+      {
+        id: "custom-id",
+        title: "custom-title",
+        url: "custom-url",
+      },
+    ]
+    ;(useFetchGif as MockedFunction<any>).mockReturnValue({
+      data: gifs,
+      loading: false,
+    })
+    const wrapper = shallow(<GifGrid category={category} />)
+    expect(wrapper).toMatchSnapshot()
+  })
+})
