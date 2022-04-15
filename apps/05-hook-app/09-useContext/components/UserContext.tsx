a662a6a51036044e7a074a118d712fcc6991415b
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-04-14 23:40:23 -0400

    feat: adding useContext v2. Ref sn

--- apps/05-hook-app/09-useContext/components/UserContext.tsx
+++ apps/05-hook-app/09-useContext/components/UserContext.tsx
@@ -3,7 +3,10 @@ import { User } from "../types"
 
 interface UserContextValue {
   user: User | {}
-  setUser: (user: Dispatch<User> | User) => void
+  setUser: (user: Dispatch<User> | User | {}) => void
 }
 
-export const UserContext = createContext<UserContextValue | null>(null)
+export const UserContext = createContext<UserContextValue>({
+  user: {},
+  setUser: () => {},
+})
