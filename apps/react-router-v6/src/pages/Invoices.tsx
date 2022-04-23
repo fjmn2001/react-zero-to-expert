5f28852b094e97da4929c7414483dbfd45af334c
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-04-23 18:59:41 -0400

    feat: react router 6 tutorial - finishing. Ref sn

--- apps/react-router-v6/src/pages/Invoices.tsx
+++ apps/react-router-v6/src/pages/Invoices.tsx
@@ -1,8 +1,9 @@
 import useInvoices from "../hooks/useInvoices"
-import { NavLink, Outlet } from "react-router-dom"
+import { NavLink, Outlet, useSearchParams } from "react-router-dom"
 
 const Invoices = () => {
   const { invoices } = useInvoices()
+  const [searchParams, setSearchParams] = useSearchParams()
 
   return (
     <div style={{ display: "flex" }}>
@@ -12,21 +13,39 @@ const Invoices = () => {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-            style={({ isActive }) => {
-              return {
-                display: "block",
-                margin: "1rem 0",
-                color: isActive ? "red" : "",
-              }
-            }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get("filter") || ""}
+          onChange={(event) => {
+            const filter = event.target.value
+            if (filter) {
+              setSearchParams({ filter })
+            } else {
+              setSearchParams({})
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            const filter = searchParams.get("filter")
+            if (!filter) return true
+            const name = invoice.name.toLowerCase()
+            return name.indexOf(filter.toLowerCase()) !== -1
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => {
+                return {
+                  display: "block",
+                  margin: "1rem 0",
+                  color: isActive ? "red" : "",
+                }
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
