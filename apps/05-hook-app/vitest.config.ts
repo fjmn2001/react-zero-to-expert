d0bfbf5c24c1b66859b572ca836183ab2fe5901e
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-05-10 22:28:39 -0400

    feat: removing enzyme and adding testing library. Ref

--- apps/05-hook-app/vitest.config.ts
+++ apps/05-hook-app/vitest.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
-// https://dev.to/mbarzeev/from-jest-to-vitest-migration-and-benchmark-23pl
-
 export default defineConfig({
   test: {
     globals: true,
